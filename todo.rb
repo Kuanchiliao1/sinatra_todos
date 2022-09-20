af9bc1549b56e258e5fd434f86808451437a6229
kuanchiliao1 <kuanchiliao@gmail.com>
2022-09-20 15:47:10 -0400

    Add feature for user to add new list

--- todo.rb
+++ todo.rb
@@ -2,16 +2,31 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis" # ERB templates
 
+configure do
+  enable :sessions # Activate Sinatra's session support
+  set :session_secret, 'secret' # If not specific, Sinatra will create random secret everytime it starts
+end
 
+# This block is ran before each request is handled
+before do
+  # Allows the rest of our code to assume session exists
+  session[:lists] ||= [] 
+end
 
 get "/lists" do
-  @list = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
+  @list = session[:lists]
   erb :lists, layout: :layout
 end
 
 get "/" do
+  redirect "/lists"
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
   redirect "/lists"
 end
