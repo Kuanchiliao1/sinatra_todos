9409ec51aeed1f7a0ba98104045d027769659f14
kuanchiliao1 <kuanchiliao@gmail.com>
2022-09-20 15:23:49 -0400

    Change homepage and fix styling on todo count

--- todo.rb
+++ todo.rb
@@ -4,10 +4,14 @@ require "tilt/erubis" # ERB templates
 
 
 
-get "/" do
+get "/lists" do
   @list = [
-    {name: "Lunch Groceries"},
-    {name: "Dinner Groceries"}
+    {name: "Lunch Groceries", todos: []},
+    {name: "Dinner Groceries", todos: []}
   ]
   erb :lists, layout: :layout
+end
+
+get "/" do
+  redirect "/lists"
 end
